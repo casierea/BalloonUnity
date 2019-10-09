d7b89dcae72aad3740a05f3133d6f11495b4d99c
Casie <dancingmongoose@gmail.com>
2019-10-09 13:02:30 -0600

    particle particle script

---
+++ BalloonUnityProject/Assets/Scripts/Particles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particles : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.Stop(); // Cannot set duration whilst Particle System is playing
+
+        var main = ps.main;
+        main.duration = 10.0f;
+
+        ps.Play();
+    }
+}
